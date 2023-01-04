40e21b6b8315f9e72eb83b708e105628aa07f1e4
alsroth <alexandre.lallis@gmail.com>
2023-01-04 20:03:32 +0100

    Centralise les éléments à parametrer

--- ExtractorProject/src/main/java/model/Settings.java
+++ ExtractorProject/src/main/java/model/Settings.java
@@ -1,7 +1,10 @@
 package model;
 
 public class Settings {
-    public static String extractedData = "D:/Data/Projets/Dev/Extractor/onepiece.txt";
+    public static String extractedData = "D:\\Data\\Projets\\Dev\\Extractor\\onepiece.txt";
     public static String path = "D:\\Logiciels\\Montage\\Ffmpeg\\ffmpeg-5.1.2-essentials_build\\bin";
     public static String directory = "D:\\Data\\Projets\\Dev\\Extractor";
+    public static String keyWord = "straw hat";
+    public static String extensionOfOrigineVideo = "mp4";
+    public static String prefixVideo = "ep";
 }
