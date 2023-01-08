023092909919413b731de8509af0df06a8d569e1
alsroth <alexandre.lallis@gmail.com>
2023-01-08 12:25:10 +0100

    Met en place des threads pour les terminaux

--- ExtractorProject/src/main/java/helper/BuilderHelper.java
+++ ExtractorProject/src/main/java/helper/BuilderHelper.java
@@ -11,22 +11,35 @@ public class BuilderHelper {
 
     private BuilderHelper() {}
 
-    public static void builderStart(ProcessBuilder builder,boolean showOutput) throws IOException {
-        builder.environment().put("Path", Settings.path);
-        // Start the process and get the process object
-        builder.directory(new File(Settings.directory));
-        builder.redirectErrorStream(true);
-        Process process = builder.start();
+    public static void builderStart(ProcessBuilder builder,boolean showOutput) {
+            Thread thread = new Thread(() -> {
+                builder.environment().put("Path", Settings.path);
+                // Start the process and get the process object
+                builder.directory(new File(Settings.directory));
+                builder.redirectErrorStream(true);
+                Process process = null;
+                try {
+                    process = builder.start();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
 
-        if (showOutput) {
-            // Get the input stream of the process and create a BufferedReader to read its output
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                if (showOutput) {
+                    // Get the input stream of the process and create a BufferedReader to read its output
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-            // Read the output of the process line by line and print it to the console
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        }
+                    // Read the output of the process line by line and print it to the console
+                    String line;
+                    while (true) {
+                        try {
+                            if ((line = reader.readLine()) == null) break;
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        System.out.println(line);
+                    }
+                }
+            });
+        thread.start();
     }
 }
