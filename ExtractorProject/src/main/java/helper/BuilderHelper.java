925e9d437748f5f89531b5dc0befef39688409ca
alsroth <alexandre.lallis@gmail.com>
2023-01-23 08:32:45 +0100

    Corrige les waitFor() sans fin

--- ExtractorProject/src/main/java/helper/BuilderHelper.java
+++ ExtractorProject/src/main/java/helper/BuilderHelper.java
@@ -2,50 +2,44 @@ package helper;
 
 import model.Settings;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.concurrent.TimeUnit;
 
 @Component
 public class BuilderHelper {
 
-    private BuilderHelper() {}
+    private BuilderHelper() {
+    }
 
-    public static void builderStart(ProcessBuilder builder,boolean showOutput) throws InterruptedException {
-            Thread thread = new Thread(() -> {
-                builder.environment().put("Path", Settings.path);
-                // Start the process and get the process object
-                builder.directory(new File(Settings.directory));
-                builder.redirectErrorStream(true);
-                Process process;
+    public static void builderStart(ProcessBuilder builder, boolean showOutput) {
+        builder.environment().put("Path", Settings.path);
+        // Start the process and get the process object
+        builder.directory(new File(Settings.directory));
+        builder.redirectErrorStream(true);
+        Process process;
+        try {
+            process = builder.start();
+            // Get the input stream of the process and create a BufferedReader to read its output
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            // Read the output of the process line by line and print it to the console
+            String line;
+            while (true) {
                 try {
-                    process = builder.start();
-                    process.waitFor(2, TimeUnit.SECONDS);
-                } catch (IOException | InterruptedException e) {
+                    if ((line = reader.readLine()) == null) break;
+                } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-
                 if (showOutput) {
-                    // Get the input stream of the process and create a BufferedReader to read its output
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-                    // Read the output of the process line by line and print it to the console
-                    String line;
-                    while (true) {
-                        try {
-                            if ((line = reader.readLine()) == null) break;
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                        System.out.println(line);
-                    }
+                    System.out.println(line);
                 }
-            });
-        thread.start();
-        thread.join();
+            }
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
